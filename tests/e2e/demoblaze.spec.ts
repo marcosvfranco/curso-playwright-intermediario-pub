c99d73f037fdb65623665a361b4dbdbd63d70d1f
Marcos Franco <v.marcos.franco@gmail.com>
2026-08-15 19:29:59 +0100

    managing cookies

--- tests/e2e/demoblaze.spec.ts
+++ tests/e2e/demoblaze.spec.ts
@@ -1,7 +1,25 @@
 import test, { expect } from '@playwright/test';
 
-test('Verificar estado logado', async ({ page }) => {
+test('Verificar estado logado', async ({ page, context }) => {
     await page.goto('https://www.demoblaze.com');
     const nameOfUser = page.locator('#nameofuser');
     await expect(nameOfUser).toHaveText('Welcome test+cursomarcos@test.com');
+    await expect(nameOfUser).toHaveText('Welcome test+cursomarcos@test.com');
+
+    await context.addCookies([
+        {
+            name: 'user-preference-theme',
+            value: 'dark',
+            domain: 'www.demoblaze.com',
+            path: '/',
+        },
+    ]);
+
+    let cookies = await context.cookies();
+    console.log('Cookies:', cookies);
+
+    await context.clearCookies();
+
+    await page.goto('https://www.demoblaze.com');
+    await expect(nameOfUser).toBeHidden();
 });
